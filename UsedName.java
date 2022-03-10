1d6b3bd286469f5606cfcd3ae394c7e9c20cbab9
isun-dev <florachoi1124@naver.com>
2022-03-10 22:16:42 +0900

    [백준]10926번

---
+++ UsedName.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/10926
+
+import java.util.Scanner;
+
+public class UsedName {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        System.out.println(name + "??!");
+
+    }
+}
